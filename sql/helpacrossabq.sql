1b0f890fbe42d1772ebe8bebf73690816f86743b
Dameon Smith <dameonsmith76@gmail.com>
2014-11-06 10:26:31 -0700

    more work completed on the profile table.

--- sql/helpacrossabq.sql
+++ sql/helpacrossabq.sql
@@ -1,5 +1,5 @@
 CREATE TABLE user(
-	userId INT UNSIGNED NOT NULL AUTO-INCREMENT,
+	userId INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	userName VARCHAR(32) NOT NULL,
 	email	VARCHAR(72)	NOT NULL,
 	passwordHash CHAR(120) NOT NULL,
@@ -7,6 +7,26 @@ CREATE TABLE user(
 	authToken CHAR(32),
 	permissions TINYINT UNSIGNED NOT NULL,
 	PRIMARY KEY(userId),
+	INDEX(permissions),
 	UNIQUE(userName),
-	UNIQUE(email),
+	UNIQUE(email)
 );
+
+CREATE TABLE profile(
+	profileId INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	userId INT UNSIGNED NOT NULL,
+	userTitle VARCHAR(64),
+	firstName VARCHAR(32) NOT NULL,
+	middleInitial CHAR(1),
+	lastName VARCHAR(32) NOT NULL,
+	biography VARCHAR(1000),
+	attention VARCHAR(64),
+	street1 VARCHAR(64),
+	street2 VARCHAR(64),
+	city VARCHAR(64),
+	state CHAR(2),
+	zipCode VARCHAR(10) NOT NULL,
+	PRIMARY KEY(profileId),
+	FOREIGN KEY (userId) REFERENCES user(userId),
+
+)
