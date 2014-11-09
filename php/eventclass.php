efca3e87b8bcd80a2d724ce477fa4f1bcd14efd6
Dameon Smith <dameonsmith76@gmail.com>
2014-11-08 19:53:25 -0700

    Started work on the Event class. https://app.asana.com/0/19128794071848/19632912981978

--- php/eventclass.php
+++ php/eventclass.php
@@ -6,4 +6,64 @@
  * Time: 08:50
  */
 
-// change for the push
+class Event {
+	private $eventId;
+
+	private $eventTitle;
+
+	private $eventDate;
+
+	private $eventLocation;
+
+	/**
+	 * @param mixed $eventId product id (or null if new object)
+	 * @param string $eventTitle event title
+	 * @param string $eventDate the date of the event
+	 * @param string $eventLocation where it is taking place
+	 **/
+	public function __construct($eventId, $eventTitle, $eventDate, $eventLocation){
+		try {
+			$this->setEventId($newEventId);
+			$this->setEventTitle($newEventTitle);
+			$this->setEventDate($newEventDate);
+			$this->setEventLocation($newEventLocation);
+		} catch(UnexpectedValueException $unexpectedValue) {
+			throw(new UnexpectedValueException("Unable to construct Event", 0, $unexpectedValue));
+		} catch(RangeException $range){
+			throw(new RangeException("Unable to construct Event", 0, $range));
+		}
+	}
+	/**
+	 * @param mixed $newEventId event id (or null if new object)
+	 * @throws UnexpectedValueException if not an integer or null
+	 * @throws RangeException if event id is not positive
+	 **/
+	public function setEventId($newEventId){
+		if($newEventId === null){
+			$this->eventId = null;
+			return;
+		}
+
+		if(filter_var($newEventId, FILTER_VALIDATE_INT) === false){
+			throw(new UnexpectedValueException("eventId $newEventId is not numeric"));
+		}
+
+		$newEventId = intval($newEventId);
+		if($newEventId <= 0) {
+			throw(new RangeException("eventId $newEventId is not positive"));
+		}
+
+		$this->eventId = $newEventId;
+	}
+
+	public function setEventId($newEventTitle){
+		$newEventTitle = trim($newEventTitle);
+		$newEventTitle = filter_var($newEventTitle, FILTER_SANITIZE_STRING);
+
+		$this->eventTitle = $newEventTitle;
+	}
+
+	public function  setEventDate($newEventDate){
+
+	}
+}
