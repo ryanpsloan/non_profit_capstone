51ee8605ce04f2e4755552145dd539171665c2b7
Dameon Smith <dameonsmith76@gmail.com>
2014-12-01 11:53:34 -0700

    fixed directory structure.

--- form_front_facing/commentform.php
+++ form_front_facing/commentform.php
@@ -7,7 +7,7 @@
 			<title>Comment</title>
 		</head>
 		<body>
-			<form id="commentForm" action="commentprocessor.php" method="POST">
+			<form id="commentForm" action="../php/form/commentprocessor.php" method="POST">
 				<label for="commentText">Type your comment:</label>
 				<input type="text" class="commentBox" name="comment" maxlength="1024">
 				<input type="datetime" name="date" value=<?php $date=date_create_from_format("Y-m-d H:i:s", "");?>>
