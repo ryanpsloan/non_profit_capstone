b490aa14b3abf6ed6804bf6409c4db29f3b24d80
Martin Luevano <moluevan@yahoo.com>
2014-12-17 11:34:00 -0700

    more fine tune edits

--- php/form/signupprocessor.php
+++ php/form/signupprocessor.php
@@ -9,6 +9,33 @@ require_once("csrf.php");
 require_once("../profile.php");
 require_once("../user.php");
 require_once("Mail.php");
+?>
+
+	<!DOCTYPE html>
+<html>
+	<head lang="en">
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta http-equiv="refresh" content="3;../../index1.html" />
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet" />
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/additional-methods.min.js"></script>
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="signUp.js"></script>
+		<link type="text/css" rel="stylesheet" href="sign.css"/>
+		<link type="text/css" rel="stylesheet" href="../index1.css"/>
+
+
+		<title>SignUp Form</title>
+	</head>
+<body>
+<?php navBarForm() ?>
+	<div class="container">
+
+<?php
+
 
 try{
 	$mysqli    = MysqliConfiguration::getMysqli();
@@ -101,3 +128,7 @@ EOF;
 		}
 
 ?>
+
+</div>
+</body>
+</html>
